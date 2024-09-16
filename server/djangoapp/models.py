aa12bba769ef0568a6f99bc5ef62708a35a18a3f
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-09-15 23:13:50 -0400

    Edited models.py, populate.py, restapis.py, urls.py, views.py, urls.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+
 # from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create a Car Make model
 class CarMake(models.Model):
-    '''
+    """
     Name
     Description
     Any other fields
     __str__ method to print a car make object
-    '''
+    """
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
@@ -21,7 +23,7 @@ class CarMake(models.Model):
 
 # Create a Car Model model `class CarModel(models.Model):
 class CarModel(models.Model):
-    '''
+    """
     Many-To-One relationship to Car Make model (One Car Make has many
     Car Models, using ForeignKey field)
     Name
@@ -30,26 +32,26 @@ class CarModel(models.Model):
     Year (IntegerField) with min value 2015 and max value 2023
     Any other fields you would like to include in car model
     __str__ method to print a car make object
-    '''
+    """
+
     # Many-to-One relationship
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        ('COUPE', 'coupe'),
-        ('HATCHBACK', 'Hatchback'),
-        ('MUSCLE', 'Muscle'),
-        ('JEEP', 'Jeep'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+        ("COUPE", "coupe"),
+        ("HATCHBACK", "Hatchback"),
+        ("MUSCLE", "Muscle"),
+        ("JEEP", "Jeep"),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2024,
-    validators=[
-        MaxValueValidator(2024),
-        MinValueValidator(2015)
-    ])
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2024,
+        validators=[MaxValueValidator(2024), MinValueValidator(2015)]
+    )
     # Other fields as needed
     color = models.CharField(max_length=10)
 
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,33 +5,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
+
 
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Network exception occurred error: {err=}, {type(err)=}")
+
 
 # request_url = sentiment_analyzer_url+"analyze/"+text
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -40,13 +41,14 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 # def post_review(data_dict):
 # Add code for posting review
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as err:
+        print(f"Network exception occurred error: {err=}, {type(err)=}")
