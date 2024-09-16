967708bdb31d3f9f23567f5aea40f2fe099ce352
Ricardo <ricardomtzleyva@hotmail.com>
2024-09-15 22:13:59 -0400

    Edited views.py, urls.py and App.js

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -97,12 +97,15 @@ def get_cars(request):
 
 #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
-    if(state == "All"):
-        endpoint = "/fetchDealers"
-    else:
-        endpoint = "/fetchDealers/"+state
-    dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
+    try:
+        if(state == "All"):
+            endpoint = "/fetchDealers"
+        else:
+            endpoint = "/fetchDealers/"+state
+        dealerships = get_request(endpoint)
+        return JsonResponse({"status":200,"dealers":dealerships})
+    except (error):
+        return JsonResponse({"status":404,"mssg":"Problem reaching DB"})    
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
